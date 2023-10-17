5a74ded568fd1cd2d7c861bc08feb783a1130336
Angela Muñoz <anggy.m@gmail.com>
2023-10-16 21:06:15 -0500

    Evidencia cargar imagen y avance final carrito

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -6,29 +6,38 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class CartService {
-  items: Product[] = [];
-  constructor(private http: HttpClient) { }
+  items: Product[]; //implementar para que el xxx cargue
+  constructor(private http: HttpClient) {
+    this.items = JSON.parse(localStorage.getItem('items') || '[]');
+  }
+  //preguntar si items existe en localStorage, si existe crear
+  //itemdesde el localStorage, no olvide usar JSOn.parse()
+  //si no exite crear items vacias
 
   addToCart(product: Product){
     this.items.push(product)
+    //guardar usando localStorage.setItem('ITEMS', JSON.stringify(this.items) )
+    localStorage.setItem('items', JSON.stringify(this.items));
   }
 
   getItems() {
-    return this.items
+    return this.items;
   }
 
   clearCart(){
-    this.items =[];
+    this.items = [];
+    localStorage.setItem('items', JSON.stringify(this.items));
     return this.items;
   }
 
   removeToCart(product: Product){
     this.items = this.items.filter(p => p.id != product.id);
+    localStorage.setItem('items', JSON.stringify(this.items));
     return this.items;
   }
 
   getShippingPrices(){
-    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json')
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
 
